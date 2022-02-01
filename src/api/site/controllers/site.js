03ba4cd87c48bf402800a0514338c06daf191a51
Sam Makhlouf <maksam7dev@gmail.com>
2022-02-01 15:03:27 +0100

    site

--- src/api/site/controllers/site.js
+++ src/api/site/controllers/site.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  contact controller
+ *  site controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::contact.contact');
+module.exports = createCoreController('api::site.site');
--- src/api/site/routes/site.js
+++ src/api/site/routes/site.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * contact router.
+ * site router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::contact.contact');
+module.exports = createCoreRouter('api::site.site');
